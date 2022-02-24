514d8408ef729483a5451ee4328eeae8c0916d19
Nursurtan <arapovnur23@gmail.com>
2022-02-24 22:52:13 +0700

    Added Refactor with Extensions

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,35 +8,43 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price:String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/USD"
-
+    var delegate: CoinManagerDelegate?
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "49A180B8-B1BA-4959-A2BD-704D98833F42"
+    
     //https://rest.coinapi.io/v1/exchangerate/BTC/USD
     //https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=49A180B8-B1BA-4959-A2BD-704D98833F42
-    let apiKey = "YOUR_API_KEY_HERE"
+    
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     func getCoinPrice(for currency: String) {
         
         
-//        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
-        
-        let urlString = baseURL + currency
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
         
         if let url = URL(string: urlString) {
             
             let session = URLSession(configuration: .default)
-            
             let task = session.dataTask(with: url) { (data,response,error) in
                 if error != nil {
-                    print(error!)
+                    self.delegate?.didFailWithError(error: error!)
                     return
                 }
                 
                 if let safeData = data {
-                    let bitcoinPrice = self.parseJSON(safeData)
+                    if let bitcoinPrice = self.parseJSON(safeData) {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
                 }
                 
             }
@@ -51,13 +59,11 @@ struct CoinManager {
             let decodedData = try decoder.decode(CoinData.self, from: data)
             let lastPrice = decodedData.rate
             
-            
             print(lastPrice)
             return lastPrice
             
         } catch {
-            
-            print(error)
+            delegate?.didFailWithError(error: error)
             return nil
         }
     }
